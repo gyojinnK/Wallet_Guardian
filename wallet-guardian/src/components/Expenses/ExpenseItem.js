46fea78bd54447d964ff5718aac8266be949b5b1
gyojink <ksmug1998@naver.com>
2023-06-24 02:02:34 +0900

    useState 추가

--- wallet-guardian/src/components/Expenses/ExpenseItem.js
+++ wallet-guardian/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("Updated!");
+        console.log(title);
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
+            {/* 함수 이름만 기입하는 이유는 해당 코드가 실행될 때 JSX코드가 평가될 때 실행되기 때문! */}
         </Card>
     );
 };
