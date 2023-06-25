b201c32d94a1440a0898d680a438e70a414f6497
gyojink <ksmug1998@naver.com>
2023-06-26 02:11:52 +0900

    양방향 바인딩 및 상태 끌어올리기

--- wallet-guardian/src/App.js
+++ wallet-guardian/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -12,25 +15,30 @@ function App() {
             id: "e2",
             title: "New TV",
             amount: 799.67,
-            date: new Date(2023, 2, 12),
+            date: new Date(2022, 2, 12),
         },
         {
             id: "e3",
             title: "Car Insurance",
             amount: 294.67,
-            date: new Date(2023, 2, 28),
+            date: new Date(2022, 2, 28),
         },
         {
             id: "e4",
             title: "New Desk (Wooden)",
             amount: 450,
-            date: new Date(2023, 5, 12),
+            date: new Date(2022, 5, 12),
         },
     ];
 
+    const addExpensesHandler = (expenses) => {
+        console.log("In App.js");
+        console.log(expenses);
+    };
+
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense onAddExpenses={addExpensesHandler} />
             <Expenses expense={expense} />
         </div>
     );
