98666cc179e522156f5840cff85b1832ab1bb2de
gyojink <ksmug1998@naver.com>
2023-06-26 12:22:32 +0900

    조건부 랜더링

--- wallet-guardian/src/components/Expenses/Expenses.js
+++ wallet-guardian/src/components/Expenses/Expenses.js
@@ -20,26 +20,18 @@ const Expenses = (props) => {
                 onChangeFilter={filterChangeHandler}
             />
 
-            <ExpenseItem
-                title={props.expense[0].title}
-                amount={props.expense[0].amount}
-                date={props.expense[0].date}
-            />
-            <ExpenseItem
-                title={props.expense[1].title}
-                amount={props.expense[1].amount}
-                date={props.expense[1].date}
-            />
-            <ExpenseItem
-                title={props.expense[2].title}
-                amount={props.expense[2].amount}
-                date={props.expense[2].date}
-            />
-            <ExpenseItem
-                title={props.expense[3].title}
-                amount={props.expense[3].amount}
-                date={props.expense[3].date}
-            />
+            {props.expense.map((expenses) => {
+                if (expenses.date.getFullYear().toString() === filteredYear) {
+                    return (
+                        <ExpenseItem
+                            key={expenses.id}
+                            title={expenses.title}
+                            amount={expenses.amount}
+                            date={expenses.date}
+                        />
+                    );
+                }
+            })}
         </Card>
     );
 };
