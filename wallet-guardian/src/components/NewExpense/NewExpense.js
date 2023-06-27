065507a6ba8f224d4651accf1b3757b512fb4791
gyojink <ksmug1998@naver.com>
2023-06-27 17:56:44 +0900

    조건부 컴포넌트 설정 및 변경

--- wallet-guardian/src/components/NewExpense/NewExpense.js
+++ wallet-guardian/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    const [viewForm, setViewForm] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -13,11 +15,24 @@ const NewExpense = (props) => {
         props.onAddExpenses(expenseData);
     };
 
-    return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
+    const viewFormHandler = () => {
+        viewForm ? setViewForm(false) : setViewForm(true);
+    };
+
+    let innerContent = (
+        <button onClick={viewFormHandler}>Add New Expense</button>
     );
+    if (viewForm) {
+        innerContent = (
+            <ExpenseForm
+                onSaveExpenseData={saveExpenseDataHandler}
+                onViewFormHandler={viewFormHandler}
+                onSetViewForm={setViewForm}
+            />
+        );
+    }
+
+    return <div className="new-expense">{innerContent}</div>;
 };
 
 export default NewExpense;
