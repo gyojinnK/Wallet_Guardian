57420c04fd3aff85e5436c6e6412d98c9afe9e2a
gyojink <ksmug1998@naver.com>
2023-06-27 17:54:51 +0900

    의미있는 태그 (li)태그로 변경

--- wallet-guardian/src/components/Expenses/ExpenseItem.js
+++ wallet-guardian/src/components/Expenses/ExpenseItem.js
@@ -5,14 +5,16 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            {/* 함수 이름만 기입하는 이유는 해당 코드가 실행될 때 JSX코드가 평가될 때 실행되기 때문! */}
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+                {/* 함수 이름만 기입하는 이유는 해당 코드가 실행될 때 JSX코드가 평가될 때 실행되기 때문! */}
+            </Card>
+        </li>
     );
 };
 
